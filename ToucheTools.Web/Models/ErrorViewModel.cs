6c5b310ea2795b9c4be222f166762a90b53b9e7c
Mike Roibu <mike@visisoft.io>
2022-06-22 10:56:04 +0100

    Set up initial web project with ASP.NET template (not React or similar to keep things simple due to needing to keep DAT file loaded back-end-side)

---
+++ ToucheTools.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ToucheTools.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
