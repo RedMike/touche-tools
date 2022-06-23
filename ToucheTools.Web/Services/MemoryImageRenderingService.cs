a1cf0491e2be6ecbfff11482bf4f81c419c5436a
Mike Roibu <mike@visisoft.io>
2022-06-23 09:48:44 +0100

    Switch image rendering to use ImageSharp for cross-platform usage

--- ToucheTools.Web/Services/MemoryImageRenderingService.cs
+++ ToucheTools.Web/Services/MemoryImageRenderingService.cs
@@ -1,7 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using ToucheTools.Models;
 
 namespace ToucheTools.Web.Services;
@@ -11,7 +9,6 @@ public interface IImageRenderingService
     byte[] RenderImage(int w, int h, byte[,] indexColorData, List<PaletteDataModel.Rgb> palette);
 }
 
-[SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
 public class MemoryImageRenderingService : IImageRenderingService
 {
     private readonly ILogger _logger;
@@ -23,30 +20,27 @@ public class MemoryImageRenderingService : IImageRenderingService
 
     public byte[] RenderImage(int w, int h, byte[,] indexColorData, List<PaletteDataModel.Rgb> palette)
     {
-        var bitmap = new Bitmap(w, h, PixelFormat.Format24bppRgb);
-        var bitmapData = bitmap.LockBits(Rectangle.FromLTRB(0, 0, w, h), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
-        IntPtr ptr = bitmapData.Scan0;
-        byte[] rgbValues = new byte[Math.Abs(bitmapData.Stride) * bitmap.Height];
-
+        var image = new Image<Rgb24>(w, h);
         HashSet<byte> seenColors = new HashSet<byte>();
-        for (var i = 0; i < h; i++)
+        image.ProcessPixelRows(pixelAccessor =>
         {
-            for (var j = 0; j < w; j++)
+            for (var i = 0; i < h; i++)
             {
-                var col = indexColorData[i, j];
-                seenColors.Add(col);
-                var idx = (i*w + j) * 3;
-                rgbValues[idx + 2] = palette[col].R;
-                rgbValues[idx + 1] = palette[col].G;
-                rgbValues[idx + 0] = palette[col].B;
+                var row = pixelAccessor.GetRowSpan(i);
+                for (var j = 0; j < w; j++)
+                {
+                    var col = indexColorData[i, j];
+                    seenColors.Add(col);
+                    var rgb = new Rgb24(palette[col].R, palette[col].G, palette[col].B);
+                    row[j] = rgb;
+                }
             }
-        }
-        _logger.LogInformation("Seen colors: {}", seenColors);
-        Marshal.Copy(rgbValues, 0, ptr, Math.Abs(bitmapData.Stride) * bitmap.Height);
-        bitmap.UnlockBits(bitmapData);
+        });
 
+        _logger.LogInformation("Seen colors: {}", seenColors);
+        
         using var mem = new MemoryStream();
-        bitmap.Save(mem, ImageFormat.Png);
+        image.SaveAsPng(mem);
         return mem.GetBuffer();
     }
 }
