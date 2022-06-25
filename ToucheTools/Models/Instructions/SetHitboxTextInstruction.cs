760d54cdf32f7d717eea9ee35b294fc0476d2b87
Mike Roibu <mike@visisoft.io>
2022-06-25 23:35:10 +0100

    More opcodes

---
+++ ToucheTools/Models/Instructions/SetHitboxTextInstruction.cs
@@ -0,0 +1,25 @@
+﻿namespace ToucheTools.Models.Instructions;
+
+public class SetHitboxTextInstruction : BaseInstruction
+{
+    public override ProgramDataModel.Opcode Opcode => ProgramDataModel.Opcode.SetHitboxText;
+    
+    public ushort Num { get; set; }
+
+    public bool IsCharacterHitbox => (Num & 0x4000) > 0;
+
+    public override void Load(BinaryReader reader)
+    {
+        Num = reader.ReadUInt16();
+    }
+    
+    protected override void ExportInternal(BinaryWriter writer)
+    {
+        writer.Write((ushort)Num);
+    }
+
+    public override string ToString()
+    {
+        return $"{Opcode:G} {(IsCharacterHitbox ? "character" : "hitbox")} {Num}";
+    }
+}
---
+++ ToucheTools/Models/Instructions/SleepInstruction.cs
@@ -0,0 +1,24 @@
+﻿namespace ToucheTools.Models.Instructions;
+
+public class SleepInstruction : BaseInstruction
+{
+    public override ProgramDataModel.Opcode Opcode => ProgramDataModel.Opcode.Sleep;
+    
+    public ushort RawCycles { get; set; }
+    public int Cycles => RawCycles * 2;
+
+    public override void Load(BinaryReader reader)
+    {
+        RawCycles = reader.ReadUInt16(); 
+    }
+    
+    protected override void ExportInternal(BinaryWriter writer)
+    {
+        writer.Write((ushort)RawCycles);
+    }
+
+    public override string ToString()
+    {
+        return $"{Opcode:G} for {Cycles} cycles";
+    }
+}
