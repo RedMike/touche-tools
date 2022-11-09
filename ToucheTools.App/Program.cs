fdedf323c2ebd9b2ddc21d9adaad94ec1026bc7d
Mike Roibu <mike@visisoft.io>
2022-11-09 23:00:01 +0000

    Refactor

--- ToucheTools.App/Program.cs
+++ ToucheTools.App/Program.cs
@@ -1,45 +1,18 @@
-﻿using System.Numerics;
-using ImGuiNET;
+﻿using ImGuiNET;
 using ToucheTools.App;
-using Veldrid;
-using Veldrid.StartupUtilities;
 
-VeldridStartup.CreateWindowAndGraphicsDevice(
-    new WindowCreateInfo(50, 50, 1280, 720, WindowState.Normal, "ImGui.NET Sample Program"),
-    new GraphicsDeviceOptions(true, null, true, ResourceBindingModel.Improved, true, true),
-    out var window,
-    out var gd);
-var controller = new ImGuiController(gd, gd.MainSwapchain.Framebuffer.OutputDescription, window.Width, window.Height);
-window.Resized += () =>
-{
-    gd.MainSwapchain.Resize((uint)window.Width, (uint)window.Height);
-    controller.WindowResized(window.Width, window.Height);
-};
+using var window = new RenderWindow("ToucheTools", 800, 600);
 
-var cl = gd.ResourceFactory.CreateCommandList();
-var clearColor = new Vector3(0.3f, 0.45f, 0.7f);
-    
-while (window.Exists)
+while (window.IsOpen())
 {
-    InputSnapshot snapshot = window.PumpEvents();
-    if (!window.Exists) { break; }
-    controller.Update(1f / 60f, snapshot); // Feed the input events to our ImGui controller, which passes them through to ImGui.
+    window.ProcessInput();
+    if (!window.IsOpen())
+    {
+        break;
+    }
     
+    //render IMGUI components
     ImGui.Text("Hello, world!");
     
-    cl.Begin();
-    cl.SetFramebuffer(gd.MainSwapchain.Framebuffer);
-    cl.ClearColorTarget(0, new RgbaFloat(clearColor.X, clearColor.Y, clearColor.Z, 1f));
-    //render
-    controller.Render(gd, cl);
-    
-    cl.End();
-    gd.SubmitCommands(cl);
-    gd.SwapBuffers(gd.MainSwapchain);
+    window.Render();
 }
-
-// Clean up Veldrid resources
-gd.WaitForIdle();
-controller.Dispose();
-cl.Dispose();
-gd.Dispose();
---
+++ ToucheTools.App/RenderWindow.cs
@@ -0,0 +1,130 @@
+﻿using System.Numerics;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+namespace ToucheTools.App;
+
+public class RenderWindow : IDisposable
+{
+    public static readonly Vector3 ClearColor = new Vector3(0.3f, 0.45f, 0.7f);
+
+    private readonly string _title;
+    private readonly int _w;
+    private readonly int _h;
+
+    private bool _windowOpen = false;
+    private Sdl2Window _window = null!;
+    private GraphicsDevice _graphicsDevice = null!;
+    private ImGuiController _controller = null!;
+    private CommandList _commandList = null!;
+    
+    private DateTime _lastUpdate = DateTime.MinValue;
+    
+    public RenderWindow(string title, int w, int h)
+    {
+        _title = title;
+        _w = w;
+        _h = h;
+
+        CreateWindow();
+    }
+
+    public bool IsOpen()
+    {
+        return _windowOpen;
+    }
+
+    public void ProcessInput()
+    {
+        InputSnapshot snapshot = _window.PumpEvents();
+        if (!_window.Exists)
+        {
+            _windowOpen = false;
+        }
+
+        var delta = NextFrameDelta();
+        _controller.Update((float)delta, snapshot);
+    }
+
+    public void Render()
+    {
+        _commandList.Begin();
+        _commandList.SetFramebuffer(_graphicsDevice.MainSwapchain.Framebuffer);
+        _commandList.ClearColorTarget(0, new RgbaFloat(ClearColor.X, ClearColor.Y, ClearColor.Z, 1f));
+        _controller.Render(_graphicsDevice, _commandList);
+        _commandList.End();
+        _graphicsDevice.SubmitCommands(_commandList);
+        _graphicsDevice.SwapBuffers(_graphicsDevice.MainSwapchain);
+    }
+
+    private void CreateWindow()
+    {
+        if (_windowOpen)
+        {
+            throw new Exception("Window already open");
+        }
+        VeldridStartup.CreateWindowAndGraphicsDevice(
+            new WindowCreateInfo(50, 50, _w, _h, WindowState.Normal, _title),
+            new GraphicsDeviceOptions(true, null, true, ResourceBindingModel.Improved, true, true),
+            out var window,
+            out var gd);
+        if (window == null || gd == null)
+        {
+            throw new Exception("Failed to create window");
+        }
+
+        _window = window;
+        _graphicsDevice = gd;
+        
+        _controller = new ImGuiController(_graphicsDevice, _graphicsDevice.MainSwapchain.Framebuffer.OutputDescription, _window.Width, _window.Height);
+        _window.Resized += WindowOnResize;
+        _window.Closing += WindowOnClose;
+        _commandList = _graphicsDevice.ResourceFactory.CreateCommandList();
+        _windowOpen = true;
+    }
+
+    private void WindowOnResize()
+    {
+        if (!_windowOpen)
+        {
+            throw new Exception("Window not open");
+        }
+        if (_controller == null)
+        {
+            throw new Exception("Missing controller");
+        }
+
+        _graphicsDevice.MainSwapchain.Resize((uint)_window.Width, (uint)_window.Height);
+        _controller.WindowResized(_window.Width, _window.Height);
+    }
+
+    private void WindowOnClose()
+    {
+        _windowOpen = false;
+    }
+    
+    private double NextFrameDelta()
+    {
+        var prevUpdate = _lastUpdate;
+        _lastUpdate = DateTime.UtcNow;
+        return (_lastUpdate - prevUpdate).TotalSeconds;
+    }
+
+    #region Dispose
+    public void Dispose()
+    {
+        try
+        {
+            _graphicsDevice.WaitForIdle();
+            _graphicsDevice.Dispose();
+            _controller.Dispose();
+            _commandList.Dispose();
+        }
+        catch (Exception)
+        {
+            //non-issue
+        }
+    }
+    #endregion
+}
