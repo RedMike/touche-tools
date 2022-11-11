a636064dc96922811ec240aff8b322c4b0e2ae39
Mike Roibu <mike@visisoft.io>
2022-11-11 16:00:08 +0000

    Sprite view refactor

--- ToucheTools.App/Program.cs
+++ ToucheTools.App/Program.cs
@@ -40,12 +40,14 @@ var programViewState = new ProgramViewState();
 var settingsWindow = new SettingsWindow(windowSettings);
 var roomViewWindow = new RoomViewWindow(window, windowSettings, activeData);
 var spriteViewSettingsWindow = new SpriteViewSettingsWindow(windowSettings, activeData, spriteViewSettings);
+var spriteViewWindow = new SpriteViewWindow(window, windowSettings, activeData, spriteViewSettings);
 
 var windows = new IWindow[]
 {
     settingsWindow,
     roomViewWindow,
-    spriteViewSettingsWindow
+    spriteViewSettingsWindow,
+    spriteViewWindow
 };
 #endregion
 
@@ -94,10 +96,6 @@ while (window.IsOpen())
     }
     #endregion
     #region Windows
-    if (windowSettings.SpriteViewOpen)
-    {
-        RenderSpriteView(activeData, spriteViewSettings);
-    }
     if (windowSettings.ProgramViewOpen)
     {
         RenderProgramViewSettings(activeData, programViewSettings);
@@ -159,58 +157,6 @@ void RenderActiveObjects(ActiveData viewModel)
     if (curSpriteId != originalSpriteId)
     {
         viewModel.SetActiveSprite(sprites[curSpriteId]);
-    }
-    
-    ImGui.End();
-}
-
-void RenderSpriteView(ActiveData viewModel, SpriteViewSettings viewSettings)
-{
-    var viewW = Constants.MainWindowWidth;
-    var viewH = 600.0f;
-    ImGui.SetNextWindowPos(new Vector2(0.0f, 200.0f));
-    ImGui.SetNextWindowSize(new Vector2(viewW, viewH));
-    ImGui.Begin("Sprite View", ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoDocking | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.AlwaysHorizontalScrollbar | ImGuiWindowFlags.AlwaysVerticalScrollbar);
-
-    var centerCursorPos = new Vector2(viewW / 2.0f, viewH / 2.0f);
-    
-    #region Room background
-    if (viewSettings.ShowRoom)
-    {
-        var ox = viewSettings.RoomOffsetX;
-        var oy = viewSettings.RoomOffsetY;
-        var (viewId, roomWidth, roomHeight, bytes) = viewModel.RoomView;
-    
-        var roomTexture = window.RenderImage(viewId, roomWidth, roomHeight, bytes);
-        var uv1 = new Vector2(ox / (float)roomWidth, oy / (float)roomHeight);
-        var uv2 = new Vector2((ox+viewW) / (float)roomWidth, (oy+viewH) / (float)roomHeight);
-        ImGui.SetCursorPos(new Vector2(0.0f, 0.0f));
-        ImGui.Image(roomTexture, new Vector2(viewW, viewH), uv1, uv2);
-    }
-    #endregion
-    
-    var (spriteViewId, spriteWidth, spriteHeight, spriteTileWidth, spriteTileHeight, spriteBytes) = viewModel.SpriteView;
-    var spriteTexture = window.RenderImage(spriteViewId, spriteWidth, spriteHeight, spriteBytes);
-
-    foreach (var (frameIndex, destX, destY, hFlip, vFlip) in viewSettings.PartsView)
-    {
-        var tileWidthRatio = (float)spriteTileWidth / spriteWidth;
-        var tileHeightRatio = (float)spriteTileHeight / spriteHeight;
-        var tilesPerRow = (int)Math.Floor((float)spriteWidth / spriteTileWidth);
-        var tileX = frameIndex % tilesPerRow;
-        var tileY = (int)Math.Floor((float)frameIndex / tilesPerRow);
-        var spriteUv1 = new Vector2(tileX * tileWidthRatio, tileY * tileHeightRatio);
-        var spriteUv2 = new Vector2((tileX + 1) * tileWidthRatio, (tileY + 1) * tileHeightRatio);
-        if (hFlip)
-        {
-            (spriteUv1.X, spriteUv2.X) = (spriteUv2.X, spriteUv1.X);
-        }
-        if (vFlip)
-        {
-            (spriteUv1.Y, spriteUv2.Y) = (spriteUv2.Y, spriteUv1.Y);
-        }
-        ImGui.SetCursorPos(centerCursorPos + new Vector2(destX, destY));
-        ImGui.Image(spriteTexture, new Vector2(spriteTileWidth, spriteTileHeight), spriteUv1, spriteUv2);   
     }
     
     ImGui.End();
---
+++ ToucheTools.App/Windows/SpriteViewWindow.cs
@@ -0,0 +1,78 @@
+﻿using System.Numerics;
+using ImGuiNET;
+using ToucheTools.App.ViewModels;
+
+namespace ToucheTools.App.Windows;
+
+public class SpriteViewWindow : IWindow
+{
+    private readonly RenderWindow _render;
+    private readonly WindowSettings _windowSettings;
+    private readonly ActiveData _activeData;
+    private readonly SpriteViewSettings _viewSettings;
+
+    public SpriteViewWindow(RenderWindow render, WindowSettings windowSettings, ActiveData activeData, SpriteViewSettings viewSettings)
+    {
+        _render = render;
+        _windowSettings = windowSettings;
+        _activeData = activeData;
+        _viewSettings = viewSettings;
+    }
+
+    public void Render()
+    {
+        if (!_windowSettings.SpriteViewOpen)
+        {
+            return;
+        }
+        
+        var viewW = Constants.MainWindowWidth;
+        var viewH = 600.0f;
+        ImGui.SetNextWindowPos(new Vector2(0.0f, 200.0f));
+        ImGui.SetNextWindowSize(new Vector2(viewW, viewH));
+        ImGui.Begin("Sprite View", ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoDocking | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.AlwaysHorizontalScrollbar | ImGuiWindowFlags.AlwaysVerticalScrollbar);
+
+        var centerCursorPos = new Vector2(viewW / 2.0f, viewH / 2.0f);
+        
+        #region Room background
+        if (_viewSettings.ShowRoom)
+        {
+            var ox = _viewSettings.RoomOffsetX;
+            var oy = _viewSettings.RoomOffsetY;
+            var (viewId, roomWidth, roomHeight, bytes) = _activeData.RoomView;
+        
+            var roomTexture = _render.RenderImage(viewId, roomWidth, roomHeight, bytes);
+            var uv1 = new Vector2(ox / (float)roomWidth, oy / (float)roomHeight);
+            var uv2 = new Vector2((ox+viewW) / (float)roomWidth, (oy+viewH) / (float)roomHeight);
+            ImGui.SetCursorPos(new Vector2(0.0f, 0.0f));
+            ImGui.Image(roomTexture, new Vector2(viewW, viewH), uv1, uv2);
+        }
+        #endregion
+        
+        var (spriteViewId, spriteWidth, spriteHeight, spriteTileWidth, spriteTileHeight, spriteBytes) = _activeData.SpriteView;
+        var spriteTexture = _render.RenderImage(spriteViewId, spriteWidth, spriteHeight, spriteBytes);
+
+        foreach (var (frameIndex, destX, destY, hFlip, vFlip) in _viewSettings.PartsView)
+        {
+            var tileWidthRatio = (float)spriteTileWidth / spriteWidth;
+            var tileHeightRatio = (float)spriteTileHeight / spriteHeight;
+            var tilesPerRow = (int)Math.Floor((float)spriteWidth / spriteTileWidth);
+            var tileX = frameIndex % tilesPerRow;
+            var tileY = (int)Math.Floor((float)frameIndex / tilesPerRow);
+            var spriteUv1 = new Vector2(tileX * tileWidthRatio, tileY * tileHeightRatio);
+            var spriteUv2 = new Vector2((tileX + 1) * tileWidthRatio, (tileY + 1) * tileHeightRatio);
+            if (hFlip)
+            {
+                (spriteUv1.X, spriteUv2.X) = (spriteUv2.X, spriteUv1.X);
+            }
+            if (vFlip)
+            {
+                (spriteUv1.Y, spriteUv2.Y) = (spriteUv2.Y, spriteUv1.Y);
+            }
+            ImGui.SetCursorPos(centerCursorPos + new Vector2(destX, destY));
+            ImGui.Image(spriteTexture, new Vector2(spriteTileWidth, spriteTileHeight), spriteUv1, spriteUv2);   
+        }
+        
+        ImGui.End();
+    }
+}
