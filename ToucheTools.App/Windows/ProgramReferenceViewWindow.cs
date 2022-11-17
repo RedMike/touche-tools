0828fd610ec03412a809110b6aa2b8e09b44679b
Mike Roibu <mike@visisoft.io>
2022-11-17 23:00:01 +0000

    Start cleanup

--- ToucheTools.App/Windows/ProgramReferenceViewWindow.cs
+++ ToucheTools.App/Windows/ProgramReferenceViewWindow.cs
@@ -60,6 +60,9 @@ public class ProgramReferenceViewWindow : IWindow
         
         ImGui.Separator();
         
+        ImGui.End();
+        return;
+        
         #region Stack
         ImGui.Text($"STK at: {state.StackPointer:D4} value: {state.StackValue}");
         if (ImGui.CollapsingHeader("Stack"))
@@ -461,146 +464,6 @@ public class ProgramReferenceViewWindow : IWindow
             }
         }
         #endregion
-        
-        // if (_programViewSettings.EvaluateUntil >= 0)
-        // {
-        //     ImGui.Text("State:");
-        //     var stateAtEvaluate = _programViewSettings.StateByInstruction[_programViewSettings.InstructionsView[_programViewSettings.EvaluateUntil].Item1];
-        //     
-        //
-        //     ImGui.Text($"STK pointer: {stateAtEvaluate.StackPointerLocation:D5} value '{stateAtEvaluate.StackPointerValue}'");
-        //     
-        //     if (stateAtEvaluate.LoadedRoom != null)
-        //     {
-        //         ImGui.Text($"Currently in room:");
-        //         ImGui.SameLine();
-        //         if (ImGui.Button($"{stateAtEvaluate.LoadedRoom}"))
-        //         {
-        //             _room.SetActive(stateAtEvaluate.LoadedRoom.Value);
-        //             _palette.SetActive(stateAtEvaluate.LoadedRoom.Value);
-        //             _windowSettings.OpenRoomView();
-        //         }
-        //     }
-        //
-        //     if (stateAtEvaluate.Flags.Count > 0)
-        //     {
-        //         if (ImGui.CollapsingHeader("Flags", ImGuiTreeNodeFlags.DefaultOpen))
-        //         {
-        //             ImGui.BeginTable("flags", 2);
-        //             ImGui.TableSetupColumn("Flag");
-        //             ImGui.TableSetupColumn("Value");
-        //             ImGui.TableHeadersRow();
-        //             foreach (var pair in stateAtEvaluate.Flags.OrderBy(p => p.Key))
-        //             {
-        //                 ImGui.TableNextColumn();
-        //                 ImGui.Text(pair.Key.ToString());
-        //                 ImGui.TableNextColumn();
-        //                 ImGui.Text(pair.Value.ToString());
-        //                 ImGui.TableNextRow();
-        //             }
-        //
-        //             ImGui.EndTable();
-        //         }
-        //     }
-        //
-        //     if (stateAtEvaluate.InventoryValuesByKeyChar.Count > 0)
-        //     {
-        //         ImGui.Text("Inventory:");
-        //         foreach (var pair in stateAtEvaluate.InventoryValuesByKeyChar.OrderBy(p => p.Key))
-        //         {
-        //             if (ImGui.CollapsingHeader($"KeyChar {pair.Key}"))
-        //             {
-        //                 ImGui.BeginTable($"items_{pair.Key}", 2);
-        //                 ImGui.TableSetupColumn("Item");
-        //                 ImGui.TableSetupColumn("Amount");
-        //                 ImGui.TableHeadersRow();
-        //                 foreach (var itemPair in pair.Value.OrderBy(p => p.Key))
-        //                 {
-        //                     ImGui.TableNextColumn();
-        //                     ImGui.Text(itemPair.Key.ToString());
-        //                     ImGui.TableNextColumn();
-        //                     ImGui.Text(itemPair.Value.ToString());
-        //                     ImGui.TableNextRow();
-        //                 }
-        //
-        //                 ImGui.EndTable();
-        //             }
-        //         }
-        //     }
-        // }
-        //
-        // ImGui.Separator();
-        //
-        // ImGui.Text("Data:");
-        // ImGui.Text("Other programs:");
-        // foreach (var program in _programViewSettings.Data.OtherPrograms.OrderBy(x => x))
-        // {
-        //     ImGui.SameLine();
-        //     if (ImGui.Button($"{program}"))
-        //     {
-        //         _program.SetActive(program);
-        //     }
-        // }
-        //
-        // ImGui.Text("Rooms:");
-        // foreach (var room in _programViewSettings.Data.LoadedRooms.OrderBy(x => x))
-        // {
-        //     ImGui.SameLine();
-        //     if (ImGui.Button($"{room}"))
-        //     {
-        //         _room.SetActive(room);
-        //         _palette.SetActive(room);
-        //         _windowSettings.OpenRoomView();
-        //     }
-        // }
-        //
-        // ImGui.Separator();
-        // ImGui.Text("Sprites (Sequence, Character):");
-        // foreach (var group in _programViewSettings.Data.SpriteSequenceCharacterCombinations
-        //              .OrderBy(x => x.Item1)
-        //              .GroupBy(x => x.Item1))
-        // {
-        //     if (ImGui.TreeNodeEx(group.Key.ToString(), ImGuiTreeNodeFlags.DefaultOpen))
-        //     {
-        //         foreach (var (_, seqId, charId) in group)
-        //         {
-        //             if (ImGui.Button($"({seqId}, {charId})"))
-        //             {
-        //                 _sprite.SetActive(group.Key);
-        //                 _sequence.SetActive(seqId);
-        //                 _character.SetActive(charId);
-        //                 _windowSettings.OpenSpriteView();
-        //             }
-        //         }
-        //         ImGui.TreePop();
-        //     }
-        // }
-        //
-        // ImGui.Separator();
-        // ImGui.Text("Character Script Offsets:");
-        // foreach (var pair in _programViewSettings.CharacterScriptOffsetView.OrderBy(p => p.Key))
-        // {
-        //     if (ImGui.Button($"{pair.Key} - {pair.Value:D5}"))
-        //     {
-        //         _programViewState.QueueScrollToOffset(pair.Value);
-        //         _programViewSettings.SetEvaluateUntil(_programViewState.OffsetToIndex[pair.Value]);
-        //     }
-        // }
-        //     
-        // ImGui.Separator();
-        // ImGui.Text("Action Script Offsets (Action, Obj1, Obj2):");
-        // foreach (var pair in _programViewSettings.ActionScriptOffsetView
-        //              .OrderBy(p => p.Key.Item1)
-        //              .ThenBy(p => p.Key.Item2)
-        //              .ThenBy(p => p.Key.Item3)
-        //          )
-        // {
-        //     if (ImGui.Button($"({pair.Key.Item1}, {pair.Key.Item2}, {pair.Key.Item3}) - {pair.Value:D5}"))
-        //     {
-        //         _programViewState.QueueScrollToOffset(pair.Value);
-        //         _programViewSettings.SetEvaluateUntil(_programViewState.OffsetToIndex[pair.Value]);
-        //     }
-        // }
         
         ImGui.End();
     }
