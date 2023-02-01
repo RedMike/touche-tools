d5aad35343bfee4424f08fa308dce18847766f29
Mike Roibu <mike@visisoft.io>
2023-02-01 18:00:06 +0000

    Override strings and export

--- ToucheTools.ModApp/Program.cs
+++ ToucheTools.ModApp/Program.cs
@@ -1,7 +1,14 @@
-﻿using ToucheTools.Loaders;
+﻿using ToucheTools.Exporters;
+using ToucheTools.Loaders;
 using ToucheTools.Models;
 
-const string doubleQuotes = "@@@";
+const string doubleQuotes = "\\\"";
+const string suffixSeparator = "__";
+#if DEBUG
+const bool debug = true;
+#else
+const bool debug = false;
+#endif
 
 Console.WriteLine($"ToucheTools.ModApp is a separate simplified editor application " +
                   $"which can be used to modify in-place a built TOUCHE.DAT file for specific " +
@@ -25,7 +32,7 @@ DatabaseModel db;
 try
 {
     var datContents = File.ReadAllBytes(datFilePath);
-    using var memStream = new MemoryStream(datContents);
+    var memStream = new MemoryStream(datContents); //don't dispose so it's accessible later for lazy loading
     var mainLoader = new MainLoader(memStream);
     mainLoader.Load(out db);
 }
@@ -62,7 +69,7 @@ do
 
 if (saving == true)
 {
-    var id = $"{datName}-{DateTime.UtcNow:yyyy-MM-dd_HH-mm-ss}";
+    var id = $"{suffixSeparator}{datName}{DateTime.UtcNow:yyyy-MM-dd_HH-mm-ss}";
     //step 1, save data as CSV/images/etc
     Console.WriteLine("");
     Console.WriteLine("The data in the TOUCHE.DAT will be saved into the same folder.");
@@ -70,7 +77,7 @@ if (saving == true)
     //save main game strings as CSV
     if (db.Text != null && db.Text.Strings.Count > 0)
     {
-        var fileName = $"game-strings_{id}.csv";
+        var fileName = $"game-strings{id}.csv";
         var filePath = Path.Combine(datPath, fileName);
         var gameStrings = db.Text.Strings;
         var lines = gameStrings.Select(p => $"{p.Key},\"{p.Value.Replace("\"", doubleQuotes)}\"");
@@ -87,7 +94,7 @@ if (saving == true)
             {
                 continue;
             }
-            var fileName = $"program-strings_{programId}_{id}.csv";
+            var fileName = $"program-strings_{programId}{id}.csv";
             var filePath = Path.Combine(datPath, fileName);
             var programStrings = program.Strings;
             var lines = programStrings.Select(p => $"{p.Key},\"{p.Value.Replace("\"", doubleQuotes)}\"");
@@ -103,22 +110,197 @@ if (saving == true)
 }
 else
 {
+    //step 2, read in saved data, overwrite DatabaseModel fields, export
     //pick which files to use
-    //TODO: list files and determine valid suffixes
-    var fileSuffix = "temp";
+    var fullSuffixSeparator = $"{suffixSeparator}{datName}";
+    var filesInFolder = Directory.EnumerateFiles(datPath).ToList();
+    var validFiles = filesInFolder.Where(f => 
+            (f.Trim().EndsWith(".csv") || f.Trim().EndsWith(".png")) &&
+            f.Contains(fullSuffixSeparator)
+        ).ToList();
+    var validSuffixes = new HashSet<string>();
+    foreach (var file in validFiles)
+    {
+        var potentialSuffix = file.Substring(
+            file.LastIndexOf(fullSuffixSeparator, StringComparison.Ordinal) + fullSuffixSeparator.Length
+            )
+            .Replace(".csv", "")
+            .Replace(".png", "");
+        validSuffixes.Add(potentialSuffix);
+    }
+
+    if (validSuffixes.Count == 0)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("ERROR: no modification files in the folder, are you sure you ran step 1 first?");
+        Console.ReadKey();
+        return;
+    }
+
+    string fileSuffix = "";
+    if (validSuffixes.Count == 1)
+    {
+        fileSuffix = validSuffixes.First();
+    }
+    else
+    {
+        if (validSuffixes.Count > 9)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("ERROR: too many different modification versions in the folder to " +
+                              "display a menu, please clear them out and leave only your current changes" +
+                              ", then try again");
+            Console.ReadKey();
+            return;
+        }
+
+        var orderedSuffixes = validSuffixes.OrderBy(s => s).ToList();
+        Console.WriteLine("");
+        Console.WriteLine("There are multiple sets of modification files in the folder, please select which one to use:");
+        var i = 1;
+        foreach (var suffix in orderedSuffixes)
+        {
+            Console.WriteLine($"    {i}. {suffix}");
+            i++;
+        }
+
+        var chosenSuffixId = 0;
+        do
+        {
+            var key = Console.ReadKey();
+            if (!char.IsAsciiDigit(key.KeyChar))
+            {
+                continue;
+            }
+
+            var digit = int.Parse(key.KeyChar.ToString());
+            if (digit == 0 || digit >= i)
+            {
+                continue;
+            }
+
+            chosenSuffixId = digit;
+        } while (chosenSuffixId == 0);
+        
+        fileSuffix = orderedSuffixes[chosenSuffixId - 1];
+    }
     
     var id = $"{fileSuffix}";
-    //step 2, read in saved data, overwrite DatabaseModel fields, export
+    var datExportFile = $"TOUCHE_{id}.DAT";
     Console.WriteLine("");
-    Console.WriteLine("The data in the TOUCHE.DAT will be modified by the files in this folder, and a " +
-                      $"TOUCHE_{id}.DAT file will be exported. " +
-                      "Your original TOUCHE.DAT file will not be changed.");
+    Console.WriteLine($"The files marked {fileSuffix} will be used for modification.");
+    Console.WriteLine("The data in the DAT file will be modified by the files in this folder, and a " +
+                      $"new {datExportFile} file will be exported. " +
+                      "Your original DAT file will not be changed.");
     
-    //TODO: load
-    //TODO: modify
-    //TODO: export
+    //game strings
+    if (db.Text != null)
+    {
+        var fileName = $"game-strings{fullSuffixSeparator}{id}.csv";
+        var path = Path.Combine(datPath, fileName);
+        if (File.Exists(path))
+        {
+            var gameStringsLines = File.ReadAllLines(path);
+            foreach (var line in gameStringsLines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+
+                if (!line.Contains(","))
+                {
+                    continue;
+                }
+
+                var stringId = int.Parse(line.Substring(0, line.IndexOf(',')));
+                var stringVal = line.Substring(line.IndexOf(',') + 1);
+                if (stringVal.StartsWith("\""))
+                {
+                    stringVal = stringVal.Substring(1);
+                }
+                if (stringVal.EndsWith("\""))
+                {
+                    stringVal = stringVal.Substring(0, stringVal.Length - 1);
+                }
+                stringVal = stringVal.Replace(doubleQuotes, "\"");
+
+                if (stringVal != db.Text.Strings[stringId])
+                {
+                    if (debug)
+                    {
+                        Console.WriteLine($"    Found game string: {stringId} = '{stringVal}' instead of '{db.Text.Strings[stringId]}'");
+                    }
+
+                    db.Text.Strings[stringId] = stringVal;
+                }
+            }
+        }
+    }
+
+    foreach (var (programId, program) in db.Programs)
+    {
+        if (program.Strings.Count == 0)
+        {
+            continue;
+        }
+        
+        var fileName = $"program-strings_{programId}{fullSuffixSeparator}{id}.csv";
+        var path = Path.Combine(datPath, fileName);
+        if (File.Exists(path))
+        {
+            var programStringsLines = File.ReadAllLines(path);
+            foreach (var line in programStringsLines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+
+                if (!line.Contains(","))
+                {
+                    continue;
+                }
+
+                var stringId = int.Parse(line.Substring(0, line.IndexOf(',')));
+                var stringVal = line.Substring(line.IndexOf(',') + 1);
+                if (stringVal.StartsWith("\""))
+                {
+                    stringVal = stringVal.Substring(1);
+                }
+                if (stringVal.EndsWith("\""))
+                {
+                    stringVal = stringVal.Substring(0, stringVal.Length - 1);
+                }
+                stringVal = stringVal.Replace(doubleQuotes, "\"");
+
+                if (stringVal != program.Strings[stringId])
+                {
+                    if (debug)
+                    {
+                        Console.WriteLine($"    Found program {programId} string: {stringId} = '{stringVal}' instead of '{program.Strings[stringId]}'");
+                    }
+
+                    program.Strings[stringId] = stringVal;
+                }
+            }
+        }
+    }
+
+    Console.WriteLine("Got all overrides, ready to export.");
+    if (debug)
+    {
+        Console.WriteLine("Debug mode, press enter to save");
+        Console.ReadLine();
+    }
     
-    Console.WriteLine($"The TOUCHE_{id}.DAT file was saved with your changes.");
+    var memStream = new MemoryStream();
+    var exporter = new MainExporter(memStream);
+    exporter.Export(db);
+    var bytes = memStream.ToArray();
+    var datExportPath = Path.Join(datPath, datExportFile);
+    File.WriteAllBytes(datExportPath, bytes);
+    
+    Console.WriteLine($"The {datExportFile} file was saved with your changes.");
     Console.ReadKey();
-    return;
 }
